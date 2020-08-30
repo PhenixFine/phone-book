ea0a977cb0b67656c44bb38d76312221707f17c8
Danny <64429863+PhenixFine@users.noreply.github.com>
2020-08-30 16:46:10 -0400

    JumpSearch.kt - I based this on my JumpSearch.kt from a side project. Changes made from original: • adjusted it to look for a String instead of an Int • removed the printing of the steps and jumps • removed linBackwards()
    Here is notes from the original:
    I based this on the Java versions of https://www.geeksforgeeks.org/jump-search/ ( their version is broken, and takes some unnecessary steps, even when fixed ) and https://hyperskill.org/learn/step/3548 which I found after trying to find a working version ( there's a lot of broken versions seemingly based on the geeksforgeeks.org version ). I took what I liked from each version and then adjusted it to keep making jumps for as long as it was practical to do so ( it stops making jumps once the number of jumps falls below 2, and then switches to either linear forward or linear backwards ).
    • variables currentRight and prevRight keep track of current index to be checked, and previous index checked.
    • the variable jump keeps track of how far to jump from current index.
    • linForward is a boolean that keeps track of whether to use linear forward or linear backwards once it is no longer practical to make any more jumps. It defaults to linear forward.
    • findWork() - calls reset(). Runs a for loop three times, checking if returnNum has been assigned a number with each loop, and breaking it if has. For the first loop it calls findBlock(), for the second it calls checkBlock(), and then it calls either linForward() or linBackwards() depending on the value of the Boolean linForward. Once done, it returns to super.find().
    • reset() - calls super.reset() and then resets currentRight, prevRight, and then assigns jump the number of jumps.
    • linDirection() - can be called to assign linear direction.
    • findBlock() - prints the current jump lenght if the Boolean printStep is true and calls printTries with the currentRight number. It then runs a loop while currentRight is less than the index number that contains find ( the number being looked for ). While looping prevRight is made equal to currentRight, and then currentRight value is increased by the jump amount ( or last index if the jump exceeds the limit ). printTries is called to increment the step number and print if printing is on. If currentRight is equal to last Index and if the value in last index is less than find, returnNum is assigned -1 and returns to calling function. Once the loop is finished it checks to see if the index to number being searched for is currentRight and if so returnNum is assigned the index number. It also checks if prevRight is greater or equal to currentRight - 1 and if so, returnNum is set to -1. It then returns to the calling function.
    • checkBlock() - val setJump is a lambda that will set jump to the new jump number. setJump() is then called. The while loop runs while jump is greater or equal to 2 and returnNum is equal to null. While that is true, it sets the val indexLast to currentRight - 1. It then sets currentRight to either prevRight + jump, or the value lastIndex, which ever is lower. It then calls findBlock() with indexLast set as the last index. Once it returns from that it calls setJump() before re-looping.
    • linForward() - steps from prevRight to currentRight while prevRight is less than the index that contains find. If prevRight equals currentRight during the loop returnNum is set to -1. Once loop is done, if prevRight is equal to the index that contains find, returnNum is set prevRight, otherwise it is set to -1.
    • linBackwards() - steps backwards from currentRight to prevRight while currentRight is greater than the index that contains find. If currentRight equals currentRight during the loop returnNum is set to -1. Once loop is done, if currentRight is equal to the index that contains find returnNum is set to currentRight, otherwise it is set to -1.

---
+++ src/main/kotlin/search/JumpSearch.kt
@@ -0,0 +1,64 @@
+package search
+
+class JumpSearch(phoneBook: Array<String>) : Search(phoneBook) {
+    private var currentRight = 0
+    private var prevRight = 0
+    private var jump = 0
+    private var checkPhone = { i: Int -> phoneBook[i].substring((0 until phoneBook[i].lastIndexOf(' '))) }
+
+    override fun findWork() {
+        reset()
+        for (i in 1..3) {
+            when (i) {
+                1 -> findBlock(lastIndex)
+                2 -> checkBlock()
+                3 -> linForward()
+            }
+            if (returnNum != null) break
+        }
+    }
+
+    private fun reset() {
+        if (currentRight != 0) currentRight = 0
+        if (prevRight != 0) prevRight = 0
+        jump = Math.sqrt((lastIndex + 1).toDouble()).toInt()
+    }
+
+    private fun findBlock(indexLast: Int) {
+        while (checkPhone(currentRight) < find) {
+            prevRight = currentRight
+            currentRight = Math.min(currentRight + jump, indexLast)
+            if (currentRight == indexLast && checkPhone(indexLast) < find) {
+                returnNum = -1
+                return
+            }
+        }
+
+        if (checkPhone(currentRight) == find) returnNum = currentRight else if (prevRight >= currentRight - 1) {
+            returnNum = -1
+        }
+    }
+
+    private fun checkBlock() {
+        val setJump = { jump = Math.sqrt(Math.max((currentRight - 1) - (prevRight), 1).toDouble()).toInt() }
+        setJump()
+
+        while (jump >= 2 && returnNum == null) {
+            val indexLast = currentRight - 1
+            currentRight = Math.min(prevRight + jump, lastIndex)
+            findBlock(indexLast)
+            setJump()
+        }
+    }
+
+    private fun linForward() {
+        while (checkPhone(prevRight) < find) {
+            prevRight++
+            if (prevRight > currentRight) {
+                returnNum = -1
+                return
+            }
+        }
+        returnNum = if (checkPhone(prevRight) == find) prevRight else -1
+    }
+}
