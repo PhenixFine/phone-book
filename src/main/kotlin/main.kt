ba989c6c75bd0e0dd9bb979ba11e24e85d68afbb
Danny <64429863+PhenixFine@users.noreply.github.com>
2020-09-12 16:12:07 -0400

    Stage 2 of 4 for JetBrains Academy Kotlin Phone Book project. • val path and phoneBook were moved to top level values. • val SORT_TIME was added to track how long things take • import() - was adjusted to format the directory.txt with names first. It was also adjusted to read files one line at a time, as I found things get really wonky when trying to reformat the text of directory.txt with reading the whole file at once. • search() - was adjusted to do a linear search and then try and do a bubble sort and jump sort ( switching back to linear search if the bubble sort takes longer than the first linear search, which it will given how slow a bubble sort is ). • sort() - attempts to do a bubble sort of the array PHONE_BOOK, stopping if the given time limit is exceeded.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,34 +1,78 @@
+import search.JumpSearch
+import search.LinearSearch
 import java.io.File
 
+private const val PATH = "src/main/kotlin/textFiles/"
+private val PHONE_BOOK = import(PATH + "directory.txt", true)
+private val SORT_TIME = TrackTime()
+
 fun main() {
-    val path = "src/main/kotlin/textFiles/"
-    val phoneBook = import(path + "directory.txt")
-    val find = import(path + "find.txt")
+    val find = import(PATH + "find.txt")
 
-    search(phoneBook, find)
+    search(find)
 }
 
-fun import(fileName: String): Array<String> {
-    val file = File(fileName)
-    return file.readText().split("\n").toTypedArray()
+fun import(fileName: String, moveNumbers: Boolean = false): Array<String> {
+    val tempList = mutableListOf<String>()
+    val numToEnd = { phoneLine: String ->
+        val num = phoneLine.indexOf(' ')
+        phoneLine.substring(num + 1) + " " + phoneLine.substring(0, num)
+    }
+
+    File(fileName).forEachLine {
+        tempList.add(if (moveNumbers) numToEnd(it) else it)
+    }
+
+    return tempList.toTypedArray()
 }
 
-fun search(phoneBook: Array<String>, find: Array<String>) {
-    var time = 0L
-    var found = 0
-    val getTime = { System.currentTimeMillis() }
-    val formatTime = { String.format("%1\$tM min. %1\$tS sec. %1\$tL ms.", time) }
+fun search(find: Array<String>) {
+    val linearSearch = LinearSearch(PHONE_BOOK)
+    val jumpSearch: JumpSearch
+    val strStart = { add: String -> "Start searching ($add)..." }
+    val formatTime = { time: Long -> String.format("%1\$tM min. %1\$tS sec. %1\$tL ms.", time) }
+    val strFound = { found: Int, time: Long -> "Found $found / ${find.size} entries. Time taken: ${formatTime(time)}" }
+    val strSortTime = { "Sorting time: " + formatTime(SORT_TIME.elapsed()) }
+    val strSearchTime = { time: Long -> "Searching time: " + formatTime(time) }
 
-    println("Start searching...")
-    time = getTime()
-    for (name in find) {
-        for (entry in phoneBook) {
-            if (entry.contains(name)) {
-                found++
-                break
+    println(strStart("linear search"))
+    for (name in find) linearSearch.find(name)
+    println(strFound(linearSearch.found(), linearSearch.timeElapsed()))
+    println("\n" + strStart("bubble sort + jump search"))
+    if (sort(linearSearch.timeElapsed() * 10)) {
+        jumpSearch = JumpSearch(PHONE_BOOK)
+        for (name in find) jumpSearch.find(name)
+        println(strFound(jumpSearch.found(), jumpSearch.timeElapsed() + SORT_TIME.elapsed()))
+        println(strSortTime())
+        println(strSearchTime(jumpSearch.timeElapsed()))
+    } else {
+        linearSearch.resetStats()
+        for (name in find) linearSearch.find(name)
+        println(strFound(linearSearch.found(), linearSearch.timeElapsed() + SORT_TIME.elapsed()))
+        println(strSortTime() + " - STOPPED, moved to linear search")
+        println(strSearchTime(linearSearch.timeElapsed()))
+    }
+}
+
+fun sort(timeLimit: Long): Boolean {
+    var changed = true
+    var stop = false
+
+    while (changed && !stop) {
+        changed = false
+        SORT_TIME.start()
+        for (num in PHONE_BOOK.indices) {
+            if (num != PHONE_BOOK.lastIndex) {
+                if (PHONE_BOOK[num] > PHONE_BOOK[num + 1]) {
+                    if (!changed) changed = true
+                    val hold = PHONE_BOOK[num]
+                    PHONE_BOOK[num] = PHONE_BOOK[num + 1]
+                    PHONE_BOOK[num + 1] = hold
+                }
             }
         }
+        SORT_TIME.stop()
+        if (SORT_TIME.elapsed() > timeLimit) stop = true
     }
-    time = getTime() - time
-    println("Found $found / ${find.size} entries. Time taken: ${formatTime()}")
+    return !stop
 }
